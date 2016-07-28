eaf39496ae5323467a2a071fbe7c0a3c24cbf5b4
HimanshuChhaya <chhaya.himanshu@gmail.com>
2016-07-28 16:53:41 +0530

    Added code for Linear Search Implementation

---
+++ LinearSearch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSandAlgoCode
+{
+    class LinearSearch
+    {
+        public int PerformLinearSearch(int[] inputArray, int key)
+        {
+            if (inputArray.Length == 0) return -1;
+            for(int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] == key)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
